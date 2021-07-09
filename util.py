d56cf7c5e888786a532170453b736ccbbe79af4b
dnazymok <dnazymok@gmail.com>
2021-07-09 13:17:27 +0300

    fix copy

--- util.py
+++ util.py
@@ -3,36 +3,40 @@ import shutil
 import os
 
 
-def copy(src: str, dst: str) -> None:
-    """Copy file or directory to the destination
+def copy(src: list, dst: str) -> None:
+    """Copy files or directory to the destination
 
     Args:
-        src: file or directory to be moved
+        src: list of files or directory to be moved
         dst: destination directory
     """
-    if os.path.isfile(src):
-        shutil.copy(src, dst)
-    elif os.path.isdir(src):
-        shutil.copytree(src, dst)
-    else:
-        print("error")  # todo log, permission error
+    for file in src:
+        if os.path.isfile(file):
+            shutil.copy(file, dst)
+        elif os.path.isdir(file):
+            path = dst + file.split("/")[-1]
+            os.mkdir(path)
+            shutil.copytree(file, path, dirs_exist_ok=True)
+        else:
+            print("error")  # todo log, permission error
 
 
-def move(src: str, dst: str) -> None:
-    """Move file or directory to the destination
+def move(src: list, dst: str) -> None:
+    """Move files or directory to the destination
 
     Args:
-        src: file or directory to be moved
+        src: list of files or directory to be moved
         dst: destination directory
     """
-    shutil.move(src, dst)
+    for file in src:
+        shutil.move(file, dst)
     # todo logging
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--operation", type=str, help="move or copy")
-    parser.add_argument("--from", dest="from_", type=str,
+    parser.add_argument("--from", nargs="*", dest="from_", type=str,
                         help="source file or directory")
     parser.add_argument("--to", type=str,
                         help="destination directory")
@@ -44,4 +48,4 @@ if __name__ == "__main__":
     elif args.operation == "move":
         move(args.from_, args.to)
     else:
-        pass  # todo log error
+        print("error")  # todo log error
